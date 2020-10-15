ccb3c6c6fc28a979786b55b20e6bfcd478e020cf
mengshouer <mengshouer@outlook.com>
2020-10-15 16:09:34 +0000

    Update antiQQMINIAPP.py

--- modules/custom/antiQQMINIAPP.py
+++ modules/custom/antiQQMINIAPP.py
@@ -8,46 +8,47 @@ sv = Service('antiMINIAPP')
 bili_url = ['www.bilibili.com/video', 'b23.tv/', '当前版本不支持该消息类型',
             '请使用最新版本手机QQ查看', '哔哩哔哩']
 
-analysis_stat = {}   # group_id: (text, is_analysis)
+analysis_stat = {}   # group_id: (msg, is_analysis)
 
 @sv.on_keyword(bili_url)
 async def bili_keyword(bot, ev):
     group_id = ev.group_id
     try:
         text = str(ev.message).strip()
-        # 避免多个机器人时重复解析
-        if group_id not in analysis_stat:
-            analysis_stat[group_id] = (text, False)
-        last_msg, is_analysis = analysis_stat[group_id]
-        if last_msg == text:
-            if is_analysis:
-                analysis_stat[group_id] = (text, False)
-                return
-            else:
-                analysis_stat[group_id] = (text, True)
-
         if "当前版本不支持该消息类型" in text and "哔哩哔哩" not in text:
             return
         elif '请使用最新版本手机QQ查看' in text and "哔哩哔哩" not in text:
             return
-        msg = await extract(text)
-        if not msg:
+        url = await extract(text)
+        if not url:
             pattern = re.compile(r'"desc":".*?"')
             desc = re.findall(pattern,text)
             i = 0
             while i < len(desc):
                 title_dict = "{"+desc[i]+"}"
                 title = eval(title_dict)
-                url = await search_bili_by_title(title['desc'])
-                if url:
-                    msg = await extract(url)
+                vurl = await search_bili_by_title(title['desc'])
+                if vurl:
+                    url = await extract(vurl)
                     break
                 i += 1
+        msg = await video_detail(url)
+
+        # 避免多个机器人解析重复推送
+        if group_id not in analysis_stat:
+            analysis_stat[group_id] = (msg, False)
+        last_msg, is_analysis = analysis_stat[group_id]
+        if last_msg == msg:
+            if is_analysis:
+                analysis_stat[group_id] = (msg, False)
+                return
+            else:
+                analysis_stat[group_id] = (msg, True)
     except Exception as e:
         msg = "Error: {}".format(type(e))
     await bot.send(ev, msg)
 
-async def extract(text):
+async def extract(text:str):
     try:
         aid = re.compile(r'(av|AV)\d+').search(text)
         bvid = re.compile(r'(BV|bv)\w+').search(text)
@@ -66,16 +67,9 @@ async def extract(text):
                 url = f'https://api.bilibili.com/x/web-interface/view?bvid={bvid[0]}'
             else:
                 url = f'https://api.bilibili.com/x/web-interface/view?aid={aid[0]}'
-        res = requests.get(url).json()['data']
-        vurl = f"URL：https://www.bilibili.com/video/av{res['aid']}\n"
-        title = f"标题：{res['title']}\n"
-        up = f"UP主：{res['owner']['name']} (https://space.bilibili.com/{res['owner']['mid']})\n"
-        desc = f"简介：{res['desc']}"
-        msg = str(vurl)+str(title)+str(up)+str(desc)
-        return msg
+        return url
     except:
         return None
-
 
 async def search_bili_by_title(title: str):
     brackets_pattern = re.compile(r'[()\[\]{}（）【】]')
@@ -96,3 +90,18 @@ async def search_bili_by_title(title: str):
     else:
         url = None
     return url
+
+async def video_detail(url):
+    try:
+        res = requests.get(url).json()['data']
+        vurl = f"URL：https://www.bilibili.com/video/av{res['aid']}\n"
+        title = f"标题：{res['title']}\n"
+        up = f"UP主：{res['owner']['name']} (https://space.bilibili.com/{res['owner']['mid']})\n"
+        desc = f"简介：{res['desc']}"
+        msg = str(vurl)+str(title)+str(up)+str(desc)
+        return msg
+    except Exception as e:
+        msg = "解析出错--Error: {}".format(type(e))
+        return msg
+
+    
