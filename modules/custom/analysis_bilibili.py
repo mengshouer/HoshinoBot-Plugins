ade7ed17f7e30a195be5f450e2f089108ae04e0f
雪狐 <mengshouer@outlook.com>
2021-01-14 16:03:38 +0000

    fix bv+av

--- modules/custom/analysis_bilibili.py
+++ modules/custom/analysis_bilibili.py
@@ -52,10 +52,10 @@ async def extract(text:str):
         b23 = re.compile(r'b23.tv\\/(\w+)').search(text)
         if not b23:
             b23 = re.compile(r'b23.tv/(\w+)').search(text)
-        if aid:
-            url = f'https://api.bilibili.com/x/web-interface/view?aid={aid[0][2:]}'
-        elif bvid:
+        if bvid:
             url = f'https://api.bilibili.com/x/web-interface/view?bvid={bvid[0]}'
+        elif aid:
+            url = f'https://api.bilibili.com/x/web-interface/view?aid={aid[0][2:]}'
         else:
             url = f'https://b23.tv/{b23[1]}'
             async with aiohttp.request('GET', url, timeout=aiohttp.client.ClientTimeout(10)) as resp:
