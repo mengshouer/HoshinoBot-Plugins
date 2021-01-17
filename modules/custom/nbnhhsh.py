a9e7bd389077d763d7e12690fcba8e23b384261e
雪狐 <mengshouer@outlook.com>
2021-01-17 14:54:55 +0000

    Update nbnhhsh.py

--- modules/custom/nbnhhsh.py
+++ modules/custom/nbnhhsh.py
@@ -1,53 +1,20 @@
 import requests
+from nonebot import on_command, CommandSession
 
-from nonebot import on_command
-
-from hoshino import Service, priv, util
-
-sv_help = '''
-nhnhhsh-能不能好好说话
-web:https://lab.magiconch.com/nbnhhsh/
-前缀sx或者直接@bot触发
-'''.strip()
-sv = Service('nbnhhsh', visible=True)
-aliases = ('sx', '缩写', 'zy', '转义', 'nhnhhsh')
-
-@sv.on_prefix(aliases)
-async def nbnhhsh(bot, ev):
-    try:
-        episode = ev.message.extract_plain_text()
-        print(episode)
-        if not episode:
-            await bot.send(ev, '请输入要转义的缩写', at_sender=True)
-            return
-        url = f'https://lab.magiconch.com/api/nbnhhsh/guess'
-        data = {
-            "text" : episode
-        }
-        r = requests.post(url,data=data,timeout=5)
-        data = r.json()[0]["trans"]
-        msg = "可能拼音缩写的是："+str(data)
-        await bot.send(ev, msg, at_sender=True)
-    except:
-        await bot.send(ev, "未查询到转义，可前往https://lab.magiconch.com/nbnhhsh/ 查询/贡献词条", at_sender=True)
-
-
-@sv.on_rex(r'^[CQ:at,qq=[0-9]([A-Za-z0-9]+)$', only_to_me=True)
-async def nbnhhsh(bot, ev):
-    try:
-        episode = ev.message.extract_plain_text()
-        print(episode)
-        if not episode:
-            await bot.send(ev, '请输入要转义的缩写', at_sender=True)
-            return
-        url = f'https://lab.magiconch.com/api/nbnhhsh/guess'
-        data = {
-            "text" : episode
-        }
-        r = requests.post(url,data=data,timeout=5)
-        data = r.json()[0]["trans"]
-        msg = "可能拼音缩写的是："+str(data)
-        await bot.send(ev, msg, at_sender=True)
-    except:
-        await bot.send(ev, "未查询到转义，可前往https://lab.magiconch.com/nbnhhsh/ 查询/贡献词条", at_sender=True)
-
+@on_command('sx', aliases=('缩写', 'zy', '转义', 'nhnhhsh', '/sx', '\sx'), only_to_me=False)
+async def nbnhhsh(session: CommandSession):
+    episode = session.current_arg_text.strip()
+    if not episode:
+        await session.send('请输入缩写的内容，缩写-nhnhhsh-能不能好好说话，web:https://lab.magiconch.com/nbnhhsh/，前缀sx触发', at_sender=True)
+    else:
+        try:
+            url = f'https://lab.magiconch.com/api/nbnhhsh/guess'
+            data = {
+                "text" : episode
+            }
+            r = requests.post(url,data=data,timeout=5)
+            data = r.json()[0]["trans"]
+            msg = "可能拼音缩写的是："+str(data)
+            await session.send(msg, at_sender=True)
+        except:
+            await session.send("未查询到转义，可前往https://lab.magiconch.com/nbnhhsh/ 查询/贡献词条", at_sender=True)
