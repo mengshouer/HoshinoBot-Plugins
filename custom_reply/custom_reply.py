a60a3c9a18aaadf3bc86fe8493d79416de8f8915
雪狐 <mengshouer@outlook.com>
2021-05-28 22:19:35 +0800

    fix bug

--- custom_reply/custom_reply.py
+++ custom_reply/custom_reply.py
@@ -13,8 +13,8 @@ async def custom_reply(ctx):
     if msg[0] in CRdata.custom_prefix or not CRdata.custom_prefix:
         for key in CRdata.data:
             if not CRdata.sensitive:
-                key = key.lower()
-                msg = msg.lower()
-            if key == msg:
+                ckey = key.lower()
+                cmsg = msg.lower()
+            if ckey == cmsg:
                 await bot.send(ctx, CRdata.data[key])
                 break
