c056f17275a6eaee1c863a88a7a8a017458223bc
mengshouer <mengshouer@outlook.com>
2021-11-17 13:41:39 +0800

    update

--- custom_reply/custom_reply.py
+++ custom_reply/custom_reply.py
@@ -1,5 +1,5 @@
 import nonebot
-import os
+import aiocqhttp
 try:
     import ujson as json
 except:
@@ -9,13 +9,14 @@ from . import *
 bot = nonebot.get_bot()
 
 @bot.on_message()
-async def custom_reply(ctx):
-    msg = ctx['raw_message']
-    if msg[0] in CRdata.custom_prefix or not CRdata.custom_prefix:
-        for key in CRdata.data:
-            if not CRdata.sensitive:
-                ckey = key.lower()
-                cmsg = msg.lower()
-            if ckey == cmsg:
-                await bot.send(ctx, CRdata.data[key])
-                break
+async def custom_reply(event: aiocqhttp.Event):
+    if event.message:
+        msg = str(event.message)
+        if msg[0] in CRdata.custom_prefix or not CRdata.custom_prefix:
+            for key in CRdata.data:
+                if not CRdata.sensitive:
+                    ckey = key.lower()
+                    cmsg = msg.lower()
+                if ckey == cmsg:
+                    await bot.send(event,CRdata.data[key])
+                    break
