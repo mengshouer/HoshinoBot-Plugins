b203424c602bd910bb63493d1477ee341bccecd2
mengshouer <mengshouer@outlook.com>
2022-06-25 14:55:18 +0800

    文件直链获取

---
+++ custom/fileLink.py
@@ -0,0 +1,36 @@
+import re
+import urllib
+
+try:
+    from hoshino import Service, MessageSegment
+
+    _sv = Service("groupFileLink")
+    sv = _sv.on_notice
+except:
+    from nonebot import on_notice, MessageSegment
+
+    sv = on_notice
+
+
+@sv("group_upload")
+async def groupFileLink(session):
+    link = session.ctx["file"]["url"]
+    file_name = session.ctx["file"]["name"]
+    size = session.ctx["file"]["size"]
+    link = re.sub(r"fname=.*", f"fname={urllib.parse.quote(file_name)}", link)
+    if link[-4:].lower() in [".jpg", ".png", ".gif", ".bmp"] and size < 31457280:
+        await session.send(MessageSegment.image(link))
+    elif (
+        link[-4:].lower()
+        in [".mp4", ".avi", ".mkv", ".rmvb", ".flv", ".wmv", ".mpg", ".mpeg"]
+        and size < 104857600
+    ):
+        await session.send(MessageSegment.video(link))
+    elif (
+        link[-4:].lower() in [".mp3", ".wav", ".wma", ".ogg", ".ape", ".flac"]
+        and size < 31457280
+    ):
+        await session.send(MessageSegment.record(link))
+    else:
+        await session.send(f"文件：{file_name}\n直链：{link}")
+
